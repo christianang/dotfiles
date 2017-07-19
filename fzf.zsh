eddf3931689f9deacaa5b166f45911cdd51513cd
Christian Ang <christian.ang@outlook.com>
2017-07-18 23:06:58 -0700

    Update for linux

--- fzf.zsh
+++ fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */home/cang/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/cang/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/cang/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "/home/cang/.fzf/shell/key-bindings.zsh"
 
