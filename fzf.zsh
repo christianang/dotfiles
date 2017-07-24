279f2a24aac15754a6288c731669dacc358ec97d
Christian Ang <cang@pivotal.io>
2017-07-24 10:17:56 -0700

    Fix issues around setup script and fzf

--- fzf.zsh
+++ fzf.zsh
@@ -1,14 +1,30 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */home/cang/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/cang/.fzf/bin"
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  # Setup fzf
+  # ---------
+  if [[ ! "${PATH}" == *${HOME}/.fzf/bin* ]]; then
+    export PATH="${PATH}:${HOME}/.fzf/bin"
+  fi
+
+  # Auto-completion
+  # ---------------
+  [[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.zsh" 2> /dev/null
+
+  # Key bindings
+  # ------------
+  source "${HOME}/.fzf/shell/key-bindings.zsh"
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  # Setup fzf
+  # ---------
+  if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+    export PATH="$PATH:/usr/local/opt/fzf/bin"
+  fi
+
+  # Auto-completion
+  # ---------------
+  [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+  # Key bindings
+  # ------------
+  source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/home/cang/.fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/home/cang/.fzf/shell/key-bindings.zsh"
-
