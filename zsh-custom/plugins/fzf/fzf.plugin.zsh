ba28c3051a03aeb22e4d7f5224bf54d3a05437ab
Christian Ang <christian.ang@outlook.com>
2018-03-13 21:07:07 -0700

    Finish up new-zshrc

--- zsh-custom/plugins/fzf/fzf.plugin.zsh
+++ zsh-custom/plugins/fzf/fzf.plugin.zsh
@@ -1,1 +1,24 @@
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  # Auto-completion
+  # ---------------
+  source "/usr/share/fzf/completion.zsh" 2> /dev/null
+
+  # Key bindings
+  # ------------
+  source "/usr/share/fzf/key-bindings.zsh"
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  # Setup fzf
+  # ---------
+  if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+    export PATH="$PATH:/usr/local/opt/fzf/bin"
+  fi
+
+  # Auto-completion
+  # ---------------
+  [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+  # Key bindings
+  # ------------
+  source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+fi
---
+++ zsh-custom/plugins/pureprompt/pureprompt.plugin.zsh
@@ -0,0 +1,4 @@
+fpath=( "$(cd "$(dirname ${0})" && pwd)" $fpath )
+
+autoload -U promptinit; promptinit
+prompt pure
