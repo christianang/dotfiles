f7820af81f1cdfcd55f49b8d2796661d95d9a9c5
Christian Ang <christian.ang@outlook.com>
2021-06-14 20:49:36 -0700

    Support either ruby 3.0.0 or 2.7.0

--- home/.config/zsh/custom/plugins/ruby/ruby.plugin.zsh
+++ home/.config/zsh/custom/plugins/ruby/ruby.plugin.zsh
@@ -1,1 +1,6 @@
-export PATH="${HOME}/.gem/ruby/3.0.0/bin:${PATH}"
+if [[ -d "${HOME}/.gem/ruby/3.0.0/bin" ]]; then
+  export PATH="${HOME}/.gem/ruby/3.0.0/bin:${PATH}"
+fi
+if [[ -d "${HOME}/.gem/ruby/2.7.0/bin" ]]; then
+  export PATH="${HOME}/.gem/ruby/2.7.0/bin:${PATH}"
+fi
