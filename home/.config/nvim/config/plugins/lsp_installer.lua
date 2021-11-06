cdce740566200655fc7ae16bb9f507d0fb4f1238
Christian Ang <christian.ang@outlook.com>
2021-11-06 14:55:42 -0700

    Use trouble for lsp diagnostics

--- home/.config/nvim/config/plugins/lsp_installer.lua
+++ home/.config/nvim/config/plugins/lsp_installer.lua
@@ -27,8 +27,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
   buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua require("telescope.builtin").lsp_document_diagnostics()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>TroubleToggle lsp_workspace_diagnostics<CR>', opts)
   buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 
 end
