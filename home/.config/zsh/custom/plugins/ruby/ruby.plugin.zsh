0f1b8ed472c1b2a2a320adeae3226814475762f7
Christian Ang <christian.ang@outlook.com>
2026-04-04 02:57:15 -0700

    Add additional ruby path

--- home/.config/zsh/custom/plugins/ruby/ruby.plugin.zsh
+++ home/.config/zsh/custom/plugins/ruby/ruby.plugin.zsh
@@ -1,3 +1,6 @@
+if [[ -d "${HOME}/.gem/ruby/3.4.0/bin" ]]; then
+  export PATH="${HOME}/.gem/ruby/3.4.0/bin:${PATH}"
+fi
 if [[ -d "${HOME}/.gem/ruby/3.2.0/bin" ]]; then
   export PATH="${HOME}/.gem/ruby/3.2.0/bin:${PATH}"
 fi
