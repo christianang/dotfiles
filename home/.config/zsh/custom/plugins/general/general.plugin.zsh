c6af3dc98f46ef1edf3598e32dc7761875c134cd
Christian Ang <christian.ang@outlook.com>
2026-04-29 18:33:26 -0700

    Ensure only a single ssh agent per user is created

--- home/.config/zsh/custom/plugins/general/general.plugin.zsh
+++ home/.config/zsh/custom/plugins/general/general.plugin.zsh
@@ -1,5 +1,25 @@
+function setup_ssh_agent() {
+    export SSH_AUTH_SOCK="/tmp/ssh-agent.$(whoami)"
+    export SSH_AGENT_PID_FILE="/tmp/ssh-agent.pid.$(whoami)"
+
+    if [[ -f "${SSH_AGENT_PID_FILE}" ]]; then
+        export SSH_AGENT_PID="$(cat ${SSH_AGENT_PID_FILE})"
+    fi
+
+    if [[ -S "${SSH_AUTH_SOCK}" ]]; then
+        return
+    fi
+
+    if [[ -n "${SSH_AGENT_PID}" ]] && [[ "$(ps -p $SSH_AGENT_PID -o comm | grep 'ssh-agent')" ]]; then
+        return
+    fi
+
+    eval "$(ssh-agent)"
+    ln -s "${SSH_AUTH_SOCK}" "/tmp/ssh-agent.$(whoami)"
+    echo -n "${SSH_AGENT_PID}" > "${SSH_AGENT_PID_FILE}"
+    export SSH_AUTH_SOCK="/tmp/ssh-agent.$(whoami)"
+}
+
 export PATH="${PATH}:/usr/bin:${HOME}/.local/bin"
 
-if [[ -z "$(ssh-add -l 2> /dev/null)" ]]; then
-    eval "$(ssh-agent)"
-fi
+setup_ssh_agent
